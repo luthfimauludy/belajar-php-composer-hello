4fd8504abbb0452b6a83d36cda366af9d1d89719
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2022-04-26 14:25:35 +0700

    Contoh Library Sederhana Composer

---
+++ src/Customer.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Maribaya\Belajar;
+
+class Customer {
+    public function __construct(private string $name) 
+    {
+    }
+
+    public function sayHello(string $name): string
+    {
+        return "Hello $name, My Name is $this->name";
+    }
+}
